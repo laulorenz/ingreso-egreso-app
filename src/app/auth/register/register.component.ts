bac19fe15ef8d7c7a7a1b8d5903e9d50fb021141
laulorenz <laulorenz@quilmes.com.ar>
2020-12-29 11:17:25 -0300

    clase 72

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -8,9 +9,23 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  public registroForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.registroForm = this.fb.group({
+      nombre: ['', Validators.required],
+      correo: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    })
   }
 
+  public crearUsuario(): void {
+    console.log(this.registroForm);
+    console.log(this.registroForm.valid);
+    console.log(this.registroForm.value);
+  }
 }
